f6332e797699ab1c6e729d04a9e6b3d14aa55a6f
joaoser222 <joaoser222@gmail.com>
2021-05-27 10:06:22 -0300

    Início do projeto

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -3,24 +3,36 @@
     <q-page-container>
       <q-page>
         <div class="q-pa-md row fullscreen items-center justify-center q-gutter-md">
-          <q-card class="bg-secondary col-lg-4 col-sm-6 ">
+          <q-card class="col-lg-4 col-md-6 col-sm-8">
+            <div class="row">
+              <div class="col-12 flex justify-center">
+                <q-card elevation="0" class="q-pa-md bg-primary" style="margin-top: -40px;height: 80px; max-width: 80px;">
+                  <img
+                    src="../assets/icons/favicon-128x128.png"
+                  >
+                </q-card>
+              </div>
+            </div>
             <q-toolbar>
               <q-toolbar-title>
-                Entrar
+                <div class="text-weight-medium text-center">Fazer Login</div>
               </q-toolbar-title>
             </q-toolbar>
-            <q-card-section class="text-white">
+            <q-card-section>
+              <div class="text-center text-subtitle2">Digite os seus dados para efetuar login no sistema</div>
               <div class="row">  
-                <div class="col-12 q-pa-xs">
-                  <q-input standout="bg-primary text-white" dense v-model="login.email"  label="Email" />
-                </div>   
-                <div class="col-12 q-pa-xs">
-                  <q-input standout="bg-primary text-white" type="password" dense v-model="login.password"  label="Senha" />
-                </div>
+                <div class="col-12 q-pa-sm">
+                  <div class="q-pb-sm">E-mail:</div>
+                  <q-input dense standout="bg-primary" elevation="0" v-model="login.email" placeholder="Digite o Email" />
+                </div> 
+                <div class="col-12 q-pa-sm">
+                  <div class="q-pb-sm">Senha:</div>
+                  <q-input dense standout="bg-primary" type="password" elevation="0" v-model="login.password" placeholder="Digite a Senha" />
+                </div> 
               </div>
             </q-card-section>
-            <q-card-actions align="right">
-              <q-btn flat @click="sendData()" label="Entrar"></q-btn>
+            <q-card-actions class="q-pa-md">
+              <q-btn unelevated color="primary" @click="sendData()" label="Entrar" class="full-width"></q-btn>
             </q-card-actions>
           </q-card>
         </div>
